3fcf7c16fcf69591d973ff134727ff8d9ca8ed94
K V Pankaj <kvpankaj1999@hotmail.com>
2021-04-06 10:36:04 +0530

    added post request

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 let landing = require('../controllers/landing');
 
 /* GET home page. */
-router.get('/', landing.get_landing);
+router.get('/',landing.get_landing);
+router.post('/',landing.submit_lead);
 
 module.exports = router;
