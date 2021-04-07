0f8e72746bbfc9e6e2778a92a7e1027496c32cab
K V Pankaj <kvpankaj1999@hotmail.com>
2021-04-07 17:23:31 +0530

    Added Passport.js for user Authentication

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+let passport = require('passport');
+let session = require('express-session');
+
+require('./passprt_setup')(passport);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,6 +22,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({secret:"Secret Code"}));
+app.use(passprt.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
